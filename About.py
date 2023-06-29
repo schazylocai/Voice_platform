cce00a8de5c60c17aef67d7b7c22b2fc45565c7c
Samuelchazy <samuel.chazy@gmail.com>
2023-06-29 11:42:05 +0400

    v2

--- About.py
+++ About.py
@@ -21,7 +21,6 @@ cancel_url="https://gptdocanalyzer.azurewebsites.net/"
 user_email = ""
 
 def first_page():
-    #global status, valid_email
     status = False
     valid_email = False
 
@@ -49,85 +48,77 @@ def first_page():
         with col3:
             st.video(video_url)
 
-    # Run Intro
-    intro()
+        # section 3
+        st.header(":red[Subscription details]")
+        st.subheader(":violet[Full access: $15 USD/Month]")
+        st.write(
+            ":violet[Subscribe today to unlock the full potential of our AI model. As a special bonus, you'll enjoy a :red[1-day free trial period] to thoroughly test its capabilities. Only if you decide to continue after the trial, your subscription will be billed at $15 USD per month.]")
 
-    def Terms():
-        # section 4
-        with st.expander(":violet[Privacy Policy]"):
-            st.caption("We are committed to protecting your privacy and ensuring the security of your personal information. This Privacy Policy outlines how we collect, use, and protect the data you provide to us when using our machine learning algorithm for document processing. Please read this policy carefully to understand our practices regarding your personal data.")
-            st.write(":violet[Collection of Personal Information:]")
-            st.caption("When you use our website and upload your documents, we may collect certain personal information, such as your name and email address, to provide you with the requested services.We do not store your documents or any processed data permanently. The documents are only kept in memory during your session and are deleted immediately afterward.")
-            st.write(":violet[Use of Personal Information]")
-            st.caption("We use the personal information collected to operate and improve our machine learning algorithm, provide customer support, and communicate with you regarding your account and subscriptions.We do not have access to or view your uploaded documents. Your privacy and the confidentiality of your data are of utmost importance to us.")
-            st.write(":violet[Security Measures:]")
-            st.caption("We take appropriate security measures to protect your personal information from unauthorized access, alteration, or disclosure.Our website is secured with SSL encryption to ensure the confidentiality of your data during transmission.")
+        def Terms():
+            # section 4
+            with st.expander(":violet[Privacy Policy]"):
+                st.caption("We are committed to protecting your privacy and ensuring the security of your personal information. This Privacy Policy outlines how we collect, use, and protect the data you provide to us when using our machine learning algorithm for document processing. Please read this policy carefully to understand our practices regarding your personal data.")
+                st.write(":violet[Collection of Personal Information:]")
+                st.caption("When you use our website and upload your documents, we may collect certain personal information, such as your name and email address, to provide you with the requested services.We do not store your documents or any processed data permanently. The documents are only kept in memory during your session and are deleted immediately afterward.")
+                st.write(":violet[Use of Personal Information]")
+                st.caption("We use the personal information collected to operate and improve our machine learning algorithm, provide customer support, and communicate with you regarding your account and subscriptions.We do not have access to or view your uploaded documents. Your privacy and the confidentiality of your data are of utmost importance to us.")
+                st.write(":violet[Security Measures:]")
+                st.caption("We take appropriate security measures to protect your personal information from unauthorized access, alteration, or disclosure.Our website is secured with SSL encryption to ensure the confidentiality of your data during transmission.")
 
-        with st.expander(":violet[Terms of Service]"):
-            st.caption("These Terms of Service outline the agreement between us and the users of our machine learning algorithm for document processing. By accessing or using our website and services, you agree to abide by these terms.")
-            st.write(":violet[Service Usage:]")
-            st.caption("Our machine learning algorithm allows users to upload and process their documents for querying or summarization purposes.The algorithm's performance may vary based on the quality and complexity of the input documents.")
-            st.write(":violet[Payment and Subscriptions]")
-            st.caption("To access our services on a monthly basis, users are required to subscribe and make the necessary payments through a secure payment website. Payments are non-refundable unless explicitly stated otherwise.")
-            st.write(":violet[Intellectual Property:]")
-            st.caption("All intellectual property rights associated with our machine learning algorithm, including any software, code, or documentation, are our property. Users are prohibited from copying, modifying, distributing, or reverse-engineering the algorithm.")
-            st.write(":violet[Limitation of Liability:]")
-            st.caption("We are not liable for any direct or indirect damages arising from the use or inability to use our services, including but not limited to loss of data, revenue, or profits. Our services are provided 'as is' without any warranties or guarantees.")
+            with st.expander(":violet[Terms of Service]"):
+                st.caption("These Terms of Service outline the agreement between us and the users of our machine learning algorithm for document processing. By accessing or using our website and services, you agree to abide by these terms.")
+                st.write(":violet[Service Usage:]")
+                st.caption("Our machine learning algorithm allows users to upload and process their documents for querying or summarization purposes.The algorithm's performance may vary based on the quality and complexity of the input documents.")
+                st.write(":violet[Payment and Subscriptions]")
+                st.caption("To access our services on a monthly basis, users are required to subscribe and make the necessary payments through a secure payment website. Payments are non-refundable unless explicitly stated otherwise.")
+                st.write(":violet[Intellectual Property:]")
+                st.caption("All intellectual property rights associated with our machine learning algorithm, including any software, code, or documentation, are our property. Users are prohibited from copying, modifying, distributing, or reverse-engineering the algorithm.")
+                st.write(":violet[Limitation of Liability:]")
+                st.caption("We are not liable for any direct or indirect damages arising from the use or inability to use our services, including but not limited to loss of data, revenue, or profits. Our services are provided 'as is' without any warranties or guarantees.")
 
-    def run_stripe():
-        # Set your Stripe API keys
-        stripe.api_key = strip_secret_key
-
-        # Redirect the user to the payment portal
-        pay = st.button(":violet[Proceed to Payment]",key='payment')
+        st.write("")
         st.write("")
         Terms()
 
-        if pay:
+    # Run Intro
+    intro()
 
-            # Initialize Stripe payment
-            session = stripe.checkout.Session.create(
-                payment_method_types=["card"],
-                line_items=[{"price": stripe_api_key, "quantity": 1}],
-                mode="subscription",
-                success_url=cancel_url,
-                cancel_url=cancel_url)
+def run_subscription():
+    subscribed_user = 'False'
+    # Run stripe
+    st.sidebar.title(":red[Want to subscribe?]")
+    # Set your Stripe API keys
+    stripe.api_key = strip_secret_key
 
-            payment_link_access = session.url
+    if pay := st.sidebar.button(":violet[Proceed to Payment]", key='payment'):
+        # Initialize Stripe payment
+        session = stripe.checkout.Session.create(
+            payment_method_types=["card"],
+            line_items=[{"price": stripe_api_key, "quantity": 1}],
+            mode="subscription",
+            success_url=cancel_url,
+            cancel_url=cancel_url)
 
-            # Redirect the user to the payment portal
-            webbrowser.open(payment_link_access,new=0,autoraise=False)
+        payment_link_access = session.url
 
-    def check_customer():
+        # Redirect the user to the payment portal
+        webbrowser.open(payment_link_access,new=0,autoraise=False)
 
-        # section 3
-        col1, col2, col3 = st.columns(3)
+    # Check customers
+    st.sidebar.title(":red[Already subscribed?]")
+    email = st.sidebar.text_input(":violet[Please enter your email address:]")
 
-        with col1:
-            subscribed = False
+    if email_button := st.sidebar.button(":violet[Submit]"):
+        customer = stripe.Customer.list(email='samuel.chazy@gmail.com')
+        if len(customer.data) == 0:
+            subscribed_user = 'True'
 
-            st.header(":red[Already subscribed?]")
-            email = st.text_input(":violet[Please enter your email address:]")
+        else:
+            st.sidebar.write(':red[You are not subscribed to this service!]')
+            subscribed_user = 'False'
 
-            if email_button := st.button(":violet[Submit]"):
-                customer = stripe.Customer.list(email='samuel.chazy@gmail.com')
-                if len(customer.data) == 0:
-                    subscribed = True
-                else:
-                    st.write(':red[You are not subscribed to this service!]')
+    return subscribed_user
 
-        with col2:
-            st.header(":red[Subscription details:]")
-            st.subheader(":violet[Full access: $15 USD/Month]")
-            st.write(
-                ":violet[Subscribe today to unlock the full potential of our AI model. As a special bonus, you'll enjoy a 1-day free trial period to thoroughly test its capabilities. Only if you decide to continue after the trial, your subscription will be billed at $15 USD per month.]")
-
-        with col3:
-            st.header(":red[Subscribe!]")
-            run_stripe()
-
-
-    check_customer()
-
-if __name__ == '__main__':
-    first_page()
+first_page()
+#run_subscription()
+st.session_state.subscribed_status = run_subscription()
--- pages/GPTapp.py
+++ pages/GPTapp.py
@@ -21,91 +21,102 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 
-file_to_upload = st.sidebar.file_uploader(label='Please select PDF files to upload:', type='pdf',
-                                              accept_multiple_files=True, key='files')
+@st.cache_data
+def launch_app():
+    file_to_upload = st.sidebar.file_uploader(label='Please select PDF files to upload:', type='pdf',
+                                                  accept_multiple_files=True, key='files')
 
-# st.caption("Developed & managed by Samuel Chazy: www.samuelchazy.com")
-st.title(":violet[GPT Document Analyzer]")
-st.write(':violet[Upload your PDF files from the left menu & start querying the documents.]')
+    # st.caption("Developed & managed by Samuel Chazy: www.samuelchazy.com")
+    st.title(":violet[GPT Document Analyzer]")
+    st.write(':violet[Upload your PDF files from the left menu & start querying the documents.]')
 
-if len(file_to_upload) > 0:
+    if len(file_to_upload) > 0:
 
-    text_list = ''
-    chunks = ''
+        text_list = ''
+        chunks = ''
 
-    for file in file_to_upload:
-        pdf_reader = PyPDF2.PdfReader(file)
-        text = "".join(page.extract_text() for page in pdf_reader.pages)
-        text_list += text
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=20,length_function=len)
-        chunks = text_splitter.split_text(text=text_list)
-        chunks = list(chunks)
+        for file in file_to_upload:
+            pdf_reader = PyPDF2.PdfReader(file)
+            text = "".join(page.extract_text() for page in pdf_reader.pages)
+            text_list += text
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=20,length_function=len)
+            chunks = text_splitter.split_text(text=text_list)
+            chunks = list(chunks)
 
-    llm = ChatOpenAI(temperature=0.7, model='gpt-4') # gpt-4 or gpt-3.5-turbo
-    memory = ConversationBufferMemory(return_messages=True)
-    embedding = OpenAIEmbeddings(openai_api_key=secret_key)
-    my_database = Chroma.from_texts(chunks, embedding)
-    retriever = my_database.as_retriever()
+        llm = ChatOpenAI(temperature=0.7, model='gpt-4') # gpt-4 or gpt-3.5-turbo
+        memory = ConversationBufferMemory(return_messages=True)
+        embedding = OpenAIEmbeddings(openai_api_key=secret_key)
+        my_database = Chroma.from_texts(chunks, embedding)
+        retriever = my_database.as_retriever()
 
-    ########## RetrievalQA from chain type ##########
-    response_template = """
-    • You are a professional in the Educational Field.
-    • Your task is to read research papers, research documents, educational journals, and conference papers.
-    • You should be analytical and reply in depth.
-    • Always reply in a polite and professional manner.
-    • Don't connect or look for answers on the internet.
-    • Only look for answers from the given documents and papers.
-    • Use conversation memory to link all question and responses together
-    • If you don't know the answer to the question, then reply: "I can't be confident about my answer because I am missing the context or some information!"
+        ########## RetrievalQA from chain type ##########
+        response_template = """
+        • You are a professional in the Educational Field.
+        • Your task is to read research papers, research documents, educational journals, and conference papers.
+        • You should be analytical and reply in depth.
+        • Always reply in a polite and professional manner.
+        • Don't connect or look for answers on the internet.
+        • Only look for answers from the given documents and papers.
+        • Use conversation memory to link all question and responses together
+        • If you don't know the answer to the question, then reply: "I can't be confident about my answer because I am missing the context or some information!"
+    
+        Divide your answer when possible into paragraphs:
+        • What is your answer to the question?
+        • How did you come up with this answer?
+        • Add citations from the document that supports the answer in bullet points at the end of your answer.
+        • Add references related to questions from the given documents only, in bullet points, each one separately, at the end of your answer.
+    
+        {context}
+    
+        Question: {question}
+    
+        Answer:
+        """
 
-    Divide your answer when possible into paragraphs:
-    • What is your answer to the question?
-    • How did you come up with this answer?
-    • Add citations from the document that supports the answer in bullet points at the end of your answer.
-    • Add references related to questions from the given documents only, in bullet points, each one separately, at the end of your answer.
+        prompt = PromptTemplate(template=response_template, input_variables=["context", "question"])
+        chain_type_kwargs = {'prompt': prompt}
+        query_model = RetrievalQA.from_chain_type(
+            llm=llm,
+            chain_type="stuff",
+            retriever=retriever,
+            chain_type_kwargs=chain_type_kwargs,
+            memory=memory,
+            verbose=False)
 
-    {context}
+        def create_text_question():
+            # Create a new text_area and button
+            with st.container():
+                st.subheader(':red[What is your query?]')
+                _user_input = st.text_area(":violet[▼]", placeholder='Enter your text...')
+                _submit_button = st.button(":violet[Submit]")
 
-    Question: {question}
+            return _user_input,_submit_button
 
-    Answer:
-    """
+        def run_model(_user_input,_submit_button):
 
-    prompt = PromptTemplate(template=response_template, input_variables=["context", "question"])
-    chain_type_kwargs = {'prompt': prompt}
-    query_model = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=retriever,
-        chain_type_kwargs=chain_type_kwargs,
-        memory=memory,
-        verbose=False)
+            if _submit_button:
+                st.divider()
+                st.write(":red[Query submitted. This may take a :red[minute] while we query the documents...]")
+                st.divider()
 
-    def create_text_question():
-        # Create a new text_area and button
-        with st.container():
-            st.subheader(':red[What is your query?]')
-            _user_input = st.text_area(":violet[▼]", placeholder='Enter your text...')
-            _submit_button = st.button(":violet[Submit]")
+                response = query_model.run(_user_input)
+                st.write(response)
 
-        return _user_input,_submit_button
+                _submit_button = False
 
-    def run_model(_user_input,_submit_button):
+            return user_input,_submit_button
 
-        if _submit_button:
-            st.divider()
-            st.write(":red[Query submitted. This may take a :red[minute] while we query the documents...]")
-            st.divider()
+        user_input,submit_button = create_text_question()
+        user_input,submit_button = run_model(user_input,submit_button)
 
-            response = query_model.run(_user_input)
-            st.write(response)
+    else:
+        st.sidebar.caption(":red[=> No file selected yet!]")
 
-            _submit_button = False
 
-        return user_input,_submit_button
-
-    user_input,submit_button = create_text_question()
-    user_input,submit_button = run_model(user_input,submit_button)
-
-else:
-    st.sidebar.caption(":red[=> No file selected yet!]")
+subscribed = False
+if "subscribed_status" in st.session_state:
+    subscribed_user = st.session_state.subscribed_status
+    if subscribed_user == 'True':
+        launch_app()
+    else:
+        st.header(':red[You are not subscribed to this service!]')
